76cbc824ed852555f059ef8b0937395591b21dd7
eashwar-git <66984736+eashwar-git@users.noreply.github.com>
2020-06-17 13:56:34 +0530

    Add files via upload

---
+++ com/java/lamda/GetValueInterface.java
@@ -0,0 +1,5 @@
+package com.java.lamda;
+
+public interface GetValueInterface {
+	double getValue();
+}
--- com/java/lamda/LamdaExpTest.java
+++ com/java/lamda/LamdaExpTest.java
@@ -3,8 +3,11 @@ package com.java.lamda;
 public class LamdaExpTest {
 	public static void main(String[] args) {
 
+	
+// 1. calculate two numbers 		
+		
 		// through Anonymous inner class
-		InterfaceCalc ex = new InterfaceCalc() {
+		CalcInterface ex = new CalcInterface() {
 			public int calc(int a, int b) {
 				return a + b;
 			}
@@ -12,11 +15,30 @@ public class LamdaExpTest {
 		System.out.println("with Anonymous inner class impl: " + ex.calc(10, 15));
 
 		// through Lamda expression
-		InterfaceCalc ex1 = (a, b) -> a + b;
+		CalcInterface ex1 = (a, b) -> a + b;
 		System.out.println("with Lambda expression :" + ex1.calc(10, 15));
 
+// 2. get a value 
+		
+		
+		
+		
+		GetValueInterface getInterface = new GetValueInterface() {
+			public double getValue() {
+				return 1.1;				
+			}
+		};
+		
+		
+		GetValueInterface getInterfaceLambda = () ->  1.1; 
+		
+		System.out.println("with Anonymous inner class impl: " +getInterface.getValue());
+		System.out.println("with Lambda expression :"+getInterfaceLambda.getValue());
+		
+		
+// 3. Reverse of a string 
 // through Anonymous inner class
-		InterfaceReverse iRev = new InterfaceReverse() {
+		ReverseInterface iRev = new ReverseInterface() {
 			public String reverse(String n) {
 				StringBuilder rv = new StringBuilder();
 				rv.append(n);
@@ -28,7 +50,7 @@ public class LamdaExpTest {
 		
 		
 		// through Lamda expression
-				InterfaceReverse intRevLamda = str ->  {
+		ReverseInterface intRevLamda = str ->  {
 						StringBuilder rv1 = new StringBuilder();
 						rv1.append(str);
 						rv1 = rv1.reverse();
@@ -36,6 +58,14 @@ public class LamdaExpTest {
 				};
 				
 				System.out.println("with Lambda expression :"+intRevLamda.reverse("abc"));
+				
+				
+				
+				
+				
+				
+				
+				
 
 	}
 }
